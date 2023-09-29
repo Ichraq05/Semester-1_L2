b0b5981a4baef6863b3951ecaa3f5f177cc66b8c
Ichraq <146119083+Ichraq05@users.noreply.github.com>
2023-09-29 18:11:36 +0200

    Add files via upload

---
+++ L2_OS/addition.sh
@@ -0,0 +1,15 @@
+read maVariable
+
+while true; do
+echo "Saisissez un nombre ou 'q' pour quitter : "
+
+    if read $maVariable = "q"; then
+        break
+    fi
+
+    old="$old $maVariable"
+
+    echo "Vous avez ajouté $maVariable à la somme"
+done
+
+./additon $old
