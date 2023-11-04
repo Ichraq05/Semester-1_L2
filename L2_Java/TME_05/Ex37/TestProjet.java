4183577cbbe1865cbaa46859c0fc6007c869e792
Ichraq <146119083+Ichraq05@users.noreply.github.com>
2023-11-04 22:45:51 +0000

    ---

--- L2_Java/TME_05/Ex37/TestProjet.java
+++ L2_Java/TME_05/Ex37/TestProjet.java
@@ -4,10 +4,18 @@ public class TestProjet {
 
         Personne p1 = new Personne();
         Personne p2 = new Personne();
-
         System.out.println("Nouvel individu : " + p1.toString());
         System.out.println("Nouvel individu : " + p2.toString());
 
         System.out.println("Nombre de personnes : " + Personne.getNbPersonnes());
+
+        System.out.println("\nNombre de Trio : " + Trio.getCpt());
+
+        Trio t1 = new Trio();
+        Trio t2 = new Trio();
+        System.out.println(t1.toString());
+        System.out.println(t2.toString());
+
+        System.out.println("Nombre de Trio : " + Trio.getCpt());
     }
 }
