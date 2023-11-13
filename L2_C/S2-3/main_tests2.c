83a45fd742ac67ccb53c82bd9aa371b06175a58f
Ichraq <146119083+Ichraq05@users.noreply.github.com>
2023-11-13 23:55:38 +0000

    ---

--- L2_C/S2-3/main_tests2.c
+++ L2_C/S2-3/main_tests2.c
@@ -1,7 +1,10 @@
+#include <assert.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
+#include <unistd.h>
+#include <strings.h> // Inclusion pour usleep
 #include "ecosys.h"
 
 #define NB_PROIES 20
@@ -20,17 +23,15 @@ int main(void) {
     for (int i = 0; i < NB_PROIES; i++) {
         int x_proie = rand() % SIZE_X;
         int y_proie = rand() % SIZE_Y;
-        energie = ENERGIE;
         liste_proie = ajouter_en_tete_animal(liste_proie, creer_animal(x_proie, y_proie, energie));
     }
 
     for(int i = 0; i < NB_PREDATEURS; i++){
         int x_predateur = rand() % SIZE_X;
         int y_predateur = rand() % SIZE_Y;
-        energie = ENERGIE;
         liste_predateur = ajouter_en_tete_animal(liste_predateur, creer_animal(x_predateur, y_predateur, energie));
     }
-
+    printf("\n=== Test d'ajout d'animal ===\n");
     // Ajoute en plus un predateur
     ajouter_animal(2,6,energie,&liste_predateur); 
 
@@ -69,6 +70,7 @@ int main(void) {
     printf("Nombre de proies : %d\n", nb_proies);
     printf("Nombre de prédateurs : %d\n", nb_predateurs);
 
+    printf("\n=== Test enlèvement animal ===\n");
     // Affiche l'état de l'écosystème
     afficher_ecosys(liste_proie, liste_predateur);
 
@@ -81,8 +83,23 @@ int main(void) {
 
     // Lisez l'écosystème depuis le fichier et mettez-le dans de nouvelles listes
     lire_ecosys("ecosystem.txt", &liste_predateur, &liste_proie);
-
+    
+    printf("\n=== Test après lecture===\n");
     afficher_ecosys(liste_proie, liste_predateur);
+
+    printf("\n=== Test de reproduction ===\n");
+    float p_reproduce = 1.0;
+    // Test reproduce
+    reproduce(&liste_predateur,p_reproduce);
+    reproduce(&liste_proie,p_reproduce);
+    afficher_ecosys(liste_proie,liste_predateur);
+
+    printf("\n=== Test de déplacement ===\n");
+    // Test bouger
+    bouger_animaux(liste_predateur);
+    bouger_animaux(liste_proie);   
+    // Affiche pour vérifier
+    afficher_ecosys(liste_proie,liste_predateur);
 
     // Libére la mémoire des listes d'animaux
     liste_proie = liberer_liste_animaux(liste_proie);
