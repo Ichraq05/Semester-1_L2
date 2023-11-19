0065afa964fb6394a0a0b5173bdb34e980c31b7f
Ichraq <146119083+Ichraq05@users.noreply.github.com>
2023-11-19 00:02:36 +0000

    ---

--- L2_Java/TME_07/Ex50/Carre.java
+++ L2_Java/TME_07/Ex50/Carre.java
@@ -2,14 +2,5 @@ public class Carre extends Rectangle{
 
     public Carre(double cotes){
         super(cotes,cotes);
-    }
-
-    public double surface(){
-        return longueur*longueur;
-    }
-
-    public double perimetre(){
-        return 4*longueur;
-    }
-    
+    }  
 }
